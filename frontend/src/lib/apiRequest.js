cb38cf54d6aa607ea15ff8df5bdba40e944775b4
Avan2021 <avandalal2007@gmail.com>
2025-05-29 10:59:12 +0530

    Delete frontend directory

--- frontend/src/lib/apiRequest.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-axios.defaults.withCredentials = true;
-
-const apiRequest = axios.create({
-    baseURL: "http://localhost:8000/api",
-    withCredentials: true,
-});
-
-export default apiRequest;
